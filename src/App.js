02e074e71ec159d8de08e1619c900694dec0e171
Muhammad Rehman <mrehman@broadpeak.pk>
2024-07-11 14:58:14 +0500

    logout button issue fix

--- src/App.js
+++ src/App.js
@@ -24,8 +24,7 @@ function App() {
     </p>
   </div>
   <div class="px-6 pt-4 pb-2">
-    <button class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Logout</button>
-   
+    <button onClick={logout} class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Logout</button>
   </div>
 </div>
         </div>
